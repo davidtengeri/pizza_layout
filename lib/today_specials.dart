11da155137cc53ea97a12e21067ccff3f36b19e9
dtengeri <dtengeri@gmail.com>
2022-03-07 15:13:04 +0100

    Added missing images

--- lib/today_specials.dart
+++ lib/today_specials.dart
@@ -7,6 +7,41 @@ class TodaySpecials extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Today Special',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: PIZZAS.length,
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  height: 230,
+                  child: Stack(
+                    children: [
+                      PizzaCard(
+                        pizza: PIZZAS[index],
+                      ),
+                      Image.asset(
+                        PIZZAS[index].imagePath,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
